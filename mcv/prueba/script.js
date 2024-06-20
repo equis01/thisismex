23c5881d7ad2fde9f5215d7efc2c4ebc46d89955
equis01 <71985026+equis01@users.noreply.github.com>
2024-06-20 12:00:02 -0600

    Add files via upload

---
+++ mcv/prueba/script.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var current = null;
+  
+  document.querySelector('#submit').addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  
+  // Hacer una solicitud POST al script de Google Apps Script
+  var url = 'https://script.google.com/macros/s/AKfycbyS8DOaLq1uWaFeVDduF0XG1huisZrZzhKbt-CQpn3NGXK7rykIqOJ2zrdGlOsU7cj2/exec';
+  var formData = new FormData();
+  formData.append('username', username);
+  formData.append('password', password);
+  
+  fetch(url, {
+  method: 'POST',
+  body: formData
+  })
+  .then(response => response.text())
+  .then(result => {
+  if (result !== 'null') {
+  // Redirigir a la URL obtenida del servidor
+  window.location.href = result;
+  } else {
+  alert('Nombre de usuario o contraseña incorrectos');
+  }
+  })
+  .catch(error => {
+  console.error('Error:', error);
+  });
+  });
+  
+  document.querySelector('#username').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+  targets: 'path',
+  strokeDashoffset: {
+  value: 0,
+  duration: 700,
+  easing: 'easeOutQuart'
+  },
+  strokeDasharray: {
+  value: '240 1386',
+  duration: 700,
+  easing: 'easeOutQuart'
+  }
+  });
+  });
+  
+  document.querySelector('#password').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+  targets: 'path',
+  strokeDashoffset: {
+  value: -336,
+  duration: 700,
+  easing: 'easeOutQuart'
+  },
+  strokeDasharray: {
+  value: '240 1386',
+  duration: 700,
+  easing: 'easeOutQuart'
+  }
+  });
+  });
+  
+  document.querySelector('#submit').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+  targets: 'path',
+  strokeDashoffset: {
+  value: -730,
+  duration: 700,
+  easing: 'easeOutQuart'
+  },
+  strokeDasharray: {
+  value: '530 1386',
+  duration: 700,
+  easing: 'easeOutQuart'
+  }
+  });
+  });
+  });
